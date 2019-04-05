7eddd11ee15b24230a5cf4d6b4267ccb0e2afad1
tanacchi <q111026d@mail.kyutech.jp>
2019-04-05 19:36:26 +0900

    Refactor with dividing scripts

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+//
+//  Setup service for Messaging API
+//
+
 const express = require("express");
 const line    = require("@line/bot-sdk");
 
@@ -8,32 +12,61 @@ const lineConfig = {
 
 const lineClient = new line.Client(lineConfig);
 const server     = express();
+server.listen(process.env.PORT || 8000);
+
+//
+//  Functions for "message" event
+//
+
+const UserKind = {
+  Hiyori: 0, Tanacchi: 1, Other: 2
+};
+
+const detectUserKind = (userId) => {
+  switch (userId) {
+    case process.env.HIYORI_USER_ID:   return UserKind.Hiyori;;
+    case process.env.TANACCHI_USER_ID: return UserKind.Tanacchi;
+    default:                           return UserKind.Other;
+  }
+};
+
+const messageCallback = (messageEvent) => {
+  const userId = messageEvent.source.userId;
+  lineClient.getProfile(userId)
+    .then((profile) => {
+      const publisherName = profile.displayName;
+      console.log(`publisher id: ${userId}`);
+      console.log(`publisher name: ${publisherName}`);
+      if (messageEvent.message.type == "text") {
+        console.log(`message content: ${messageEvent.message.text}`);
+      }
+      const userKind = detectUserKind(userId);
+      switch (userKind)
+      {
+        case UserKind.Hiyori:
+          lineClient.replyMessage(messageEvent.replyToken, {type: "text", text: "あいあいよちよち"});
+          break;
+        case UserKind.Tanacchi:
+          lineClient.pushMessage(process.env.HIYORI_USER_ID, {type: "text", text: messageEvent.message.text}); 
+          lineClient.replyMessage(messageEvent.replyToken, {type: "text", text: "Message was sent."});
+          break;
+        default:
+          lineClient.replyMessage(messageEvent.replyToken, {type: "text", text: "Hello"});
+      }
+    });
+};
+
+//
+//  Request managers
+//
 
 server.post("/webhook", line.middleware(lineConfig), (req, res) => {
   res.sendStatus(200);
 
   for (const event of req.body.events) {
     if (event.type == "message") {
-      const use_id = event.source.userId;
-      lineClient.getProfile(use_id)
-        .then((profile) => {
-          const publisher_name = profile.displayName;
-          console.log(`publisher id: ${use_id}`);
-          console.log(`publisher name: ${publisher_name}`);
-          if (event.message.type == "text") {
-            console.log(`message content: ${event.message.text}`);
-          }
-          if (use_id == process.env.HIYORI_USER_ID) {
-            lineClient.replyMessage(event.replyToken, {type: "text", text: "あいあいよちよち"});
-          } else if (use_id == process.env.TANACCHI_USER_ID) {
-            lineClient.pushMessage(process.env.HIYORI_USER_ID, {type: "text", text: event.message.text}); 
-            lineClient.replyMessage(event.replyToken, {type: "text", text: "Message was send"});
-          } else {
-            lineClient.replyMessage(event.replyToken, {type: "text", text: "Hello"});
-          }
-        });
+      messageCallback(event);
     }
   }
 });
 
-server.listen(process.env.PORT || 8000);
