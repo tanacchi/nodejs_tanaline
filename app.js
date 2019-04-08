8fde3b962203438b338aefee7d7229039f659502
tanacchi <q111026d@mail.kyutech.jp>
2019-04-08 20:12:41 +0900

    Test getting ipadress

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const line    = require("@line/bot-sdk");
+const os      = require("os");
 
 const lineConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
@@ -9,8 +10,30 @@ const lineConfig = {
 const lineClient = new line.Client(lineConfig);
 const server     = express();
 
+server.listen(process.env.PORT || 8000);
+
 server.post("/webhook", line.middleware(lineConfig), (req, res) => {
   res.sendStatus(200);
-});
 
-server.listen(process.env.PORT || 8000);
+  const ifaces = os.networkInterfaces();
+  // Object.keys(ifaces).forEach(function (ifname) {
+    // var alias = 0;
+
+    // ifaces[ifname].forEach(function (iface) {
+      // if ('IPv4' !== iface.family || iface.internal !== false) {
+        // // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+        // return;
+      // }
+
+      // if (alias >= 1) {
+        // // this single interface has multiple ipv4 addresses
+        // console.log(ifname + ':' + alias, iface.address);
+      // } else {
+        // // this interface has only one ipv4 adress
+        // console.log(ifname, iface.address);
+      // }
+      // ++alias;
+    // });
+  // });
+  console.log(ifaces.eth0[0].address);
+});
