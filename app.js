637ac9805b017ce08b108551956f3991a4f77af0
tanacchi <q111026d@mail.kyutech.jp>
2019-04-11 22:41:58 +0900

    Modify to test database connection

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const line    = require("@line/bot-sdk");
+const pg      = require("pg");
+const config  = require("config");
+
+const pool = new pg.Pool(config.db.postgres);
 
 const lineConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
@@ -11,6 +15,18 @@ const server     = express();
 
 server.post("/webhook", line.middleware(lineConfig), (req, res) => {
   res.sendStatus(200);
+  pool.connect((err, client, done) => {
+    client.query(query, param, (err, result) => {
+      done();
+      if(err) {
+        callback(err);
+      }
+      else {
+        console.log("sql: " + query + ", param: " + param + ", count:" + result.rowCount);
+        callback(null, result);
+      }
+    });
+  });
 });
 
 server.listen(process.env.PORT || 8000);
